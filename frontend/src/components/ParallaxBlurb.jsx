25ac09e9e89f753d08ad0ad543e11b1b3b725e57
Mitchell Lagaras <mookielagaras@gmail.com>
2025-03-28 17:04:24 -0400

    started horizontal scroll container component. tried to resolve spacing issue between parallax layer. Gonna have A LOT of fun refactoring this for mobile

--- frontend/src/components/ParallaxBlurb.jsx
+++ frontend/src/components/ParallaxBlurb.jsx
@@ -2,7 +2,7 @@ import { ParallaxLayer } from '@react-spring/parallax';
 
 const ParallaxBlurb = ({ subtitle, text }) => {
   return (
-    <ParallaxLayer className={"aboutBlurb"} factor= {0.6} offset={0.9} speed={1}>
+    <ParallaxLayer className={"aboutBlurb"} factor= {0.5} offset={0.9} speed={1}>
       <div className={"page-text-container"}>
         <div className={"page-text-div"}>
          <p className={"page-text-subtitle"}>{subtitle}</p>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,17 +4,19 @@ import ParallaxImage from '../components/ParallaxImage';
 import OpaqueParallax from '../components/OpaqueParallax';
 import ParallaxTitle from '../components/ParallaxTitle';
 import ParallaxBlurb from '../components/ParallaxBlurb';
+import HorizontalScrollContainer from '../components/HorizontalScrollContainer';
 
 const HomePage = () => {
 
   return (
-    <Parallax className={"home-page-parallax"} pages={4}>
+    <Parallax className={"home-page-parallax"} pages={2.5}>
       {/* OPENING SCREN WITH TITLE */}
       <ParallaxImage source={flamingMountain} />
       <OpaqueParallax />
       <ParallaxTitle title={"ΣΤΡΑΤΕΙΑ"} />
       {/* ABOUT BLURB */}
       <ParallaxBlurb subtitle={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} text={"Nam id gravida sapien. Mauris libero lorem, rutrum sit amet diam sed, varius viverra ligula. Aenean sed pulvinar ante. Praesent ac ligula sollicitudin, aliquet tortor ut, elementum turpis. Suspendisse in porta nibh. Cras malesuada velit sapien, vel vulputate ligula vehicula ut. Sed sollicitudin pretium lacus, eget fermentum diam rhoncus nec."} />
+      <HorizontalScrollContainer offset={0.9} speed={1} factor={0.7} />
     </Parallax>
   )
 }
